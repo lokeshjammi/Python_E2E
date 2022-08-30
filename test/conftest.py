f3b700ca731747bdec3f5cd6feb2b20f8ed7064d
Lokesh Jammi <jammilokesh3@gmail.com>
2022-08-31 03:59:01 +0530

    Page Object files commit

--- test/conftest.py
+++ test/conftest.py
@@ -3,12 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action = "store", default = "chrome"
+    )
 @pytest.fixture(scope="class")
 def setup(request):
-    service = Service("D:\Python_Workspace\Python_Automation\Drivers\Edge\msedgedriver.exe")
-    driver = webdriver.Edge(service=service)
-    driver.maximize_window()
-    driver.get("https://www.google.com")
-    request.cls.driver = driver
-    yield
-    driver.quit()
+    browser_name = request.config.getOption("--browser_name")
+    if browser_name == "edge":
+        service = Service("D:\Python_Workspace\Python_Automation\Drivers\Edge\msedgedriver.exe")
+        driver = webdriver.Edge(service=service)
+        driver.maximize_window()
+        driver.get("https://www.google.com")
+        request.cls.driver = driver
+        yield
+        driver.quit()
+    elif browser_name == "chrome":
+        service = Service("D:\Python_Workspace\Python_Automation\Drivers\chrome\chrome.exe")
+        driver = webdriver.Chrome(service=service)
+        driver.maximize_window()
+        driver.get("https://www.google.com")
+        return driver
--- test/test_e2e.py
+++ test/test_e2e.py
@@ -5,5 +5,6 @@ from selenium.webdriver.common.by import By
 from Utilities.BaseClass import BaseClass
 
 class TestOne(BaseClass):
-    def test_e2e(self, setup):
-        
+    def test_e2e(self):
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("Myntra")
+        time.sleep(5)
